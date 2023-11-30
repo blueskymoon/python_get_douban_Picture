37538ed23dfa5bb257480fdf9499256bff8e7189
我辈岂是蓬蒿人 <yueliangdeshouhuzhe@outlook.com>
2023-11-30 17:08:50 +0800

    第二个版本推送，错误版本

--- picture_刘涛/chromdriver测试.py
+++ picture_刘涛/chromdriver测试.py
@@ -6,5 +6,5 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-chrome_driver_path = 'D:\Chrome低版本103内部版本\Chrome_1170582\chrome-win\chromedriver.exe'  # 将路径替换为您的ChromeDriver路径
-browser = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+chrome_driver_path = 'D:\Chrome低版本103内部版本\Chrome_1170582\chrome-win\chromedriver.exe'  # 请替换为您的ChromeDriver的实际路径
+browser = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver_path)
